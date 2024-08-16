5bf18a548d7074f6d799e2113c13697f90814623
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2024-08-16 13:27:47 -0300

    Add files via upload

--- Vision/js/formCadastro.js
+++ Vision/js/formCadastro.js
@@ -5,10 +5,12 @@ addBookButton.addEventListener('click', function(event) {
     event.preventDefault();
 
     dialog.showModal();
+    modal.classList.add('show');
 });
 
 dialog.addEventListener('click', function(event) {
     if (event.target === dialog) {
         dialog.close();
+        modal.classList.add('show');
     }
 });
